36a7f356ba4f9bbcc64c5d1eeaac9706ce09c6a9
xintong <xintong000927@gmail.com>
2025-05-17 22:18:44 +0900

    built DB connection

---
+++ python/db/items.sql
@@ -0,0 +1,8 @@
+
+    CREATE TABLE IF NOT EXISTS items (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,   
+        name TEXT NOT NULL,
+        category TEXT NOT NULL,
+        image_name TEXT
+    );
+    
--- python/main.py
+++ python/main.py
@@ -1,24 +1,29 @@
 import os
 import logging
 import pathlib
-from fastapi import FastAPI, Form, HTTPException, Depends
+from fastapi import FastAPI, Form, HTTPException, Depends, UploadFile, File
 from fastapi.responses import FileResponse
 from fastapi.middleware.cors import CORSMiddleware
 import sqlite3
 from pydantic import BaseModel
-from contextlib import asynccontextmanager
+from contextlib import contextmanager, asynccontextmanager
+
+#---newly added---
+import hashlib
+from typing import Optional
+from fastapi.concurrency import run_in_threadpool
 
 
 # Define the path to the images & sqlite3 database
-images = pathlib.Path(__file__).parent.resolve() / "images"
-db = pathlib.Path(__file__).parent.resolve() / "db" / "mercari.sqlite3"
+images_dir = pathlib.Path(__file__).parent.resolve() / "images"
+DB = pathlib.Path(__file__).parent.resolve() / "db" / "mercari.sqlite3"
 
-
+@contextmanager
 def get_db():
-    if not db.exists():
-        yield
+    if not DB.exists():
+        raise FileNotFoundError(f"Database {DB} does not exist.")
 
-    conn = sqlite3.connect(db)
+    conn = sqlite3.connect(DB)
     conn.row_factory = sqlite3.Row  # Return rows as dictionaries
     try:
         yield conn
@@ -28,7 +33,14 @@ def get_db():
 
 # STEP 5-1: set up the database connection
 def setup_database():
-    pass
+    sql_schema = pathlib.Path(__file__).parent.resolve() / "db" / "items.sql"
+    with get_db() as db:
+
+        with sql_schema.open("r") as f:
+            schema = f.read()
+
+        db.cursor().execute(schema)
+        db.commit()
 
 
 @asynccontextmanager
@@ -41,7 +53,7 @@ app = FastAPI(lifespan=lifespan)
 
 logger = logging.getLogger("uvicorn")
 logger.level = logging.INFO
-images = pathlib.Path(__file__).parent.resolve() / "images"
+images_dir = pathlib.Path(__file__).parent.resolve() / "images"
 origins = [os.environ.get("FRONT_URL", "http://localhost:3000")]
 app.add_middleware(
     CORSMiddleware,
@@ -67,37 +79,54 @@ class AddItemResponse(BaseModel):
 
 # add_item is a handler to add a new item for POST /items .
 @app.post("/items", response_model=AddItemResponse)
-def add_item(
+async def add_item(
     name: str = Form(...),
-    db: sqlite3.Connection = Depends(get_db),
+    category: str = Form(...),
+    image_file: UploadFile = File(None),
 ):
     if not name:
         raise HTTPException(status_code=400, detail="name is required")
 
-    insert_item(Item(name=name))
+    image_name = None
+    if image_file is not None:
+        # save img
+        read = await image_file.read()
+        image_name = hashlib.sha256(read).hexdigest()
+        with (images_dir / f"{image_name}.jpg").open('wb') as f:
+            f.write(read)
+
+    insert_item(Item(name=name, category=category, image_name=f"{image_name}.jpg"))
+
     return AddItemResponse(**{"message": f"item received: {name}"})
+
+
 
 
 # get_image is a handler to return an image for GET /images/{filename} .
 @app.get("/images/{image_name}")
 async def get_image(image_name):
     # Create image path
-    image = images / image_name
+    image = images_dir / image_name
 
     if not image_name.endswith(".jpg"):
         raise HTTPException(status_code=400, detail="Image path does not end with .jpg")
 
     if not image.exists():
         logger.debug(f"Image not found: {image}")
-        image = images / "default.jpg"
+        image = images_dir / "default.jpg"
 
     return FileResponse(image)
 
 
 class Item(BaseModel):
     name: str
+    category: str
+    image_name: Optional[str] = None
 
-
+# STEP 4-2: add an implementation to store an item
 def insert_item(item: Item):
-    # STEP 4-2: add an implementation to store an item
-    pass
+    table_name = "items"
+    with get_db() as db:
+        db.cursor().execute(f"Insert into {table_name} (id, name, category, image_name) "
+                            f"values (NULL, ?, ?, ?)", (item.name, item.category, item.image_name))
+        db.commit()
